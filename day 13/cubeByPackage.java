92d42993262ec20cc3ad7813a3e5a14e4c5cd050
Alanbabu147 <89576006+Alanbabu147@users.noreply.github.com>
2021-09-15 20:32:04 +0530

    Add files via upload

---
+++ day 13/cubeByPackage.java
@@ -0,0 +1,10 @@
+import pack.CubeOfNum1;
+import java.util.*;
+class cubeByPackage
+{
+	public static void main(String[] args)
+	{
+		CubeOfNum1 obj1=new CubeOfNum1();
+		obj1.CubeOfNum1 (5);
+	} 
+}
